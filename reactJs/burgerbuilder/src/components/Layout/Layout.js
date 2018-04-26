0a8db2d470dc216339b519d54a034e68bd594b7b
SoundharNatarajan <alonesoundhar@gmail.com>
2018-04-26 20:53:55 +0530

    Finished Layout Structure

--- reactJs/burgerbuilder/src/components/Layout/Layout.js
+++ reactJs/burgerbuilder/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
 import Aux from '../../hoc/Aux';
+import classes from  './Layout.css';
 
 const Layout = (props) => {
   return (
     <Aux>
       <div>Toolbar, Sidebar, Backdrop</div>
-      <main>
+      <main className={classes.Content}>
         {props.children}
       </main>
     </Aux>
