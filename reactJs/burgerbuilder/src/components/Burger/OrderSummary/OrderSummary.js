383e08a1d562417d4cfeb875d85f0f6e0b3da4aa
SoundharNatarajan <alonesoundhar@gmail.com>
2018-04-27 18:49:58 +0530

    BurgerBuilder UI Completed

--- reactJs/burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
+++ reactJs/burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
+import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  // can be functional component doesnt have to be a class component.
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
+
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return (<li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}:</span>           {props.ingredients[igKey]}</li>);
+          <span style={{textTransform: 'capitalize'}}>{igKey}:</span>           {this.props.ingredients[igKey]}</li>);
     });
-  return (
-    <Aux>
-      <h3>Your Burger</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p>Continue to Checkout!</p>
-    </Aux>
-  )
+    return (
+      <Aux>
+        <h3>Your Burger</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout!</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    );
+  }
+
+
+
 }
 
-export default orderSummary;
+export default OrderSummary;
--- reactJs/burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ reactJs/burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
@@ -71,6 +71,14 @@ export class BurgerBuilder extends Component {
     this.setState({purchasing: true});
   }
 
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+
+  purchaseContinueHandler = () => {
+    alert('You Continue!');
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -80,8 +88,13 @@ export class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
-          <OrderSummary ingredients={this.state.ingredients}/ >
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+          <OrderSummary 
+            ingredients= {this.state.ingredients}
+            price= {this.state.totalPrice}
+            purchaseCancelled = {this.purchaseCancelHandler}
+            purchaseContinued = {this.purchaseContinueHandler}
+            />
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls 
