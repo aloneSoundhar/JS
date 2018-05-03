9d359edc81dc1a30421ac3f48999fd70da20bd45
SoundharNatarajan <alonesoundhar@gmail.com>
2018-05-03 05:38:33 +0530

    Changed Folder Strucuture

--- FullStack/Emaily/Server/index.js
+++ FullStack/Emaily/Server/index.js
@@ -1,5 +1,9 @@
 // Imported Express
-const express = require("express");
+const express = require('express');
+// Importing Passport
+const passport = require('passport');
+// Google Authentuication
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // Inititalised Express in app
 const app = express();
@@ -8,6 +12,13 @@ const app = express();
 app.get("/", (req, res) => {
   res.send({ hi: "There" });
 });
+
+//   clientID: '985355583629-1rdpnv4cvnp6lbb4s7q3c7j25ur5e1r4.apps.googleusercontent.com',
+//   clientSecret: '9ZCmtmwGSDiNeN00PTe2kBWl' 
+
+passport.use(new GoogleStrategy({
+
+}, ))
 
 // Set a static port for listening requests
 app.listen(5000);
